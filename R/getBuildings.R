61c1c164523988d97051e9bbca786e611480b080
Jonathan Smith <jonathan@peoplesized.com>
2018-11-20 12:24:30 +0200

    try returning just wd

--- R/getBuildings.R
+++ R/getBuildings.R
@@ -2,11 +2,11 @@ library(sp)
 library(geojsonio)
 
 getBuildings <- function(cluster_id) {
-  print(getwd())
-  load("prediction_building_subset_sp.RData")
-  prediction_building_subset_sp$poly = as.vector(prediction_building_subset_sp$which_poly)
-  prediction_building_subset_sp$which_poly <- NULL
-  plot_buildings <- prediction_building_subset_sp[prediction_building_subset_sp$cluster ==
-                                                    cluster_id,]
-  geojsonio::geojson_json(plot_buildings)
+  return(getwd())
+  # load("prediction_building_subset_sp.RData")
+  # prediction_building_subset_sp$poly = as.vector(prediction_building_subset_sp$which_poly)
+  # prediction_building_subset_sp$which_poly <- NULL
+  # plot_buildings <- prediction_building_subset_sp[prediction_building_subset_sp$cluster ==
+  #                                                   cluster_id,]
+  # geojsonio::geojson_json(plot_buildings)
 }
