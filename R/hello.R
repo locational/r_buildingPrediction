254154a6a68892382fb3ee491893ce8782d188b9
Jonathan Smith <jonathan@peoplesized.com>
2018-11-20 11:32:15 +0200

    matrix col problem

--- R/hello.R
+++ R/hello.R
@@ -1,20 +1,13 @@
 library(sp)
-library(jsonlite)
-
-# library(raster)
-# library(rgdal)
-# library(sf)
-# library(geojsonio)
-
-
+library(geojsonio)
 
 hello <- function() {
-  print("Hello, world!")
+  return("Hello, world!")
 }
 
 getBuildings <- function(cluster_id) {
   load("R/prediction_building_subset_sp.RData")
   plot_buildings <- prediction_building_subset_sp[prediction_building_subset_sp$cluster ==
                                                     cluster_id,]
-
+  geojsonio::geojson_json(plot_buildings)
 }
