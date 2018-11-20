3727429282a88033b455cedb01bee9b050809916
Jonathan Smith <jonathan@peoplesized.com>
2018-11-20 12:21:45 +0200

    print wd

--- R/getBuildings.R
+++ R/getBuildings.R
@@ -2,7 +2,8 @@ library(sp)
 library(geojsonio)
 
 getBuildings <- function(cluster_id) {
-  load("R/prediction_building_subset_sp.RData")
+  print(getwd())
+  load("prediction_building_subset_sp.RData")
   prediction_building_subset_sp$poly = as.vector(prediction_building_subset_sp$which_poly)
   prediction_building_subset_sp$which_poly <- NULL
   plot_buildings <- prediction_building_subset_sp[prediction_building_subset_sp$cluster ==
