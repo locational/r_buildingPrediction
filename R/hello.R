da45b57d07494010826992e0e17dcfac42a58b44
Jonathan Smith <jonathan@peoplesized.com>
2018-11-20 11:54:47 +0200

    split functions to files

--- R/hello.R
+++ R/hello.R
@@ -1,15 +1,4 @@
-library(sp)
-library(geojsonio)
 
 hello <- function() {
   return("Hello, world!")
-}
-
-getBuildings <- function(cluster_id) {
-  load("R/prediction_building_subset_sp.RData")
-  prediction_building_subset_sp$poly = as.vector(prediction_building_subset_sp$which_poly)
-  prediction_building_subset_sp$which_poly <- NULL
-  plot_buildings <- prediction_building_subset_sp[prediction_building_subset_sp$cluster ==
-                                                    cluster_id,]
-  geojsonio::geojson_json(plot_buildings)
 }
