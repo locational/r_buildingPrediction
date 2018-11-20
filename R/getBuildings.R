ba36b17362fe96e750cc4845014f8d0c8c8dfd96
Jonathan Smith <jonathan@peoplesized.com>
2018-11-20 12:35:20 +0200

    and again

--- R/getBuildings.R
+++ R/getBuildings.R
@@ -2,10 +2,14 @@ library(sp)
 library(geojsonio)
 
 getBuildings <- function(cluster_id) {
-  load("./R/prediction_building_subset_sp.RData")
-  prediction_building_subset_sp$poly = as.vector(prediction_building_subset_sp$which_poly)
-  prediction_building_subset_sp$which_poly <- NULL
-  plot_buildings <- prediction_building_subset_sp[prediction_building_subset_sp$cluster ==
-                                                    cluster_id,]
-  geojsonio::geojson_json(plot_buildings)
+  data_path = "R/prediction_building_subset_sp.RData"
+  wd = getwd()
+  exists = file.exists(data_path)
+  return(list(wd = wd, exists = exists))
+  # load(data_path)
+  # prediction_building_subset_sp$poly = as.vector(prediction_building_subset_sp$which_poly)
+  # prediction_building_subset_sp$which_poly <- NULL
+  # plot_buildings <- prediction_building_subset_sp[prediction_building_subset_sp$cluster ==
+  #                                                   cluster_id,]
+  # geojsonio::geojson_json(plot_buildings)
 }
